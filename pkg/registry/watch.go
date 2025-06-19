cd0f896df5e1821a3c0b979121f799909395342a
Shiming Zhang <wzshiming@hotmail.com>
2025-06-19 14:58:38 +0800

    Clean up

--- pkg/registry/watch.go
+++ pkg/registry/watch.go
@@ -3,6 +3,7 @@ package registry
 import (
 	"sync/atomic"
 
+	metainternalversion "k8s.io/apimachinery/pkg/apis/meta/internalversion"
 	"k8s.io/apimachinery/pkg/watch"
 )
 
@@ -10,14 +11,17 @@ type leaseStorageWatch struct {
 	f       *leaseStorage
 	stopped atomic.Bool
 	ch      chan watch.Event
+
+	namespace string
+	options   *metainternalversion.ListOptions
 }
 
 func (w *leaseStorageWatch) Stop() {
 	w.stopped.Store(true)
 
-	w.f.Lock()
+	w.f.watchesMut.Lock()
 	delete(w.f.watches, w)
-	w.f.Unlock()
+	w.f.watchesMut.Unlock()
 }
 
 func (w *leaseStorageWatch) IsStopped() bool {
